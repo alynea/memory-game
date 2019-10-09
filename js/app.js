d6a9d12638c8454c430d580f77b30fb1e40d006b
Alyne Abraham <52169769+alynea@users.noreply.github.com>
2019-10-09 10:03:12 -0500

    Built Game Logic
    - Select card
        - Card changes from black
          background to blue
          background with a
          symbol
    
    -Compare selected cards
        - if matching cards, put
          cards in a new list and
          change background to
          green and remains open.
        - if non-matching cards,
          return the cards back to
          black background and
          game continue

--- js/app.js
+++ js/app.js
@@ -24,38 +24,82 @@ function shuffle(array) {
 
     return array;
 }
-function compareCards(){
+function compareCards() {
     let card1 = currentSelection[0];
     let card2 = currentSelection[1];
-    if (card1.children[0].className == card2.children[0].className){
+    if (card1.children[0].className == card2.children[0].className) {
         return true;
-    } else{
+    } else {
         return false;
-    }    
+    }
 }
 
-function handleMatchedCards(){
+
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
+async function handleMatchedCards() {
+    await delay(500);
     matchingSelection.push(...currentSelection);
     console.log(matchingSelection);
     currentSelection = [];
-    console.log(currentSelection);    
+    matchingSelection.forEach(card => {
+        card.classList.add("match");
+        card.classList.remove("open","show");
+    });
+}
+
+async function handleUnmatchedCards() {
+    await delay(2000);
+    currentSelection.forEach(card => {
+        console.log("removing card open show");
+        card.classList.remove("open", "show");
+    });
+    currentSelection = [];
+}
+
+function checkIfCardSelected(sourceElement){
+   
+    let cardExists = false;
+    matchingSelection.forEach(card => {
+        if(card.id == sourceElement.id){
+            cardExists = true;
+        }
+    });
+
+    currentSelection.forEach(card => {
+       if(card.id == sourceElement.id){
+           cardExists = true;
+       }
+    });
+    return cardExists;
+    
+
 }
 
 /*
  * set up the event listener for a card. If a card is clicked:*/
 let allCards = document.querySelectorAll('.card');
-allCards.forEach(card=>{
-    card.addEventListener('click',function(e){
-        e.srcElement.className = 'card open show';        
+allCards.forEach(card => {
+    card.addEventListener('click', function (e) {
+        
         console.log(e.srcElement.children[0].className);
-        currentSelection.push(e.srcElement);
-        if (currentSelection.length == 2){
-            if (compareCards() == true ){
+
+        if (checkIfCardSelected(e.srcElement) == false){
+            e.srcElement.className = 'card open show';
+            currentSelection.push(e.srcElement);
+        }
+        if (currentSelection.length == 2) {
+            if (compareCards() == true) {
                 handleMatchedCards();
-            }            
+                console.log(matchingSelection.srcElement);
+            } else {
+                handleUnmatchedCards();
+            }
         }
     });
-    
+
 });
 let currentSelection = [];
 let matchingSelection = [];
@@ -70,6 +114,7 @@ let matchingSelection = [];
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + 
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
