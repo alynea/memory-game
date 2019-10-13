4023cae402268d85a1ad2c89e9615f5c008c1b0c
Alyne Abraham <52169769+alynea@users.noreply.github.com>
2019-10-13 16:38:41 -0500

    Timer and updated Refresh Button
    -Set up a Count up timer to
      start counting the tie it takes
      to complete the game.
    -Refresh button updated:
      Clicking the button
      refreshes the deck, the
      timer and the stars to start a
      new game.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-/*
- * Create a list that holds all of your cards*/
+/* Create a list that holds all of your cards*/
 let currentSelection = [];
 let matchingSelection = [];
 let moves = 0;
+let timerStarted = false;
+let timer = 0;
+let startTime;
+let displayTime = ""
 
 let deck = [];
 deck.push("fa fa-diamond");
@@ -44,71 +47,7 @@ function createNewDeck() {
 
 
 }
-function compareCards() {
-    let card1 = currentSelection[0];
-    let card2 = currentSelection[1];
-    if (card1.children[0].className == card2.children[0].className) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function incrementMove() {
-    moves++;
-    document.getElementById("currentMove").innerText = moves;
-}
-
-
-function setStarRating() {
-    console.log(moves)
-    if (moves > 10) {
-        let thirdStar = document.getElementById("star3")
-        thirdStar.children[0].classList.add("star-white");
-    } 
-    if (moves > 20) {
-        let secondStar = document.getElementById("star2")
-        secondStar.children[0].classList.add("star-white");
-
-    }
-}
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
-
-async function handleMatchedCards() {
-    await delay(500);
-    matchingSelection.push(...currentSelection);
-    console.log(matchingSelection);
-    currentSelection = [];
-    matchingSelection.forEach(card => {
-        card.classList.add("match");
-        card.classList.remove("open", "show");
-        goodShake(card);
-    });
-
-}
-
-async function handleUnmatchedCards() {
-    await delay(500);
-    currentSelection.forEach(card => {
-        badShake(card);
-    });
-    await delay(500);
-    currentSelection.forEach(card => {
-
-        card.classList.remove("open", "show");
-
-    });
-    currentSelection = [];
-}
-
-function badShake(card) {
-    // perform a horizontal shake when the cards match for 1sec//
-    card.classList.add("animated", "wobble");
-
-
-}
+// Game features//
 
 function checkIfCardSelected(sourceElement) {
 
@@ -136,50 +75,202 @@ function checkTwoCardLimit() {
     }
 }
 
+//Compare Cards//
+
+function compareCards() {
+    let card1 = currentSelection[0];
+    let card2 = currentSelection[1];
+    if (card1.children[0].className == card2.children[0].className) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Increment Move//
+
+function incrementMove() {
+    moves++;
+    document.getElementById("currentMove").innerText = moves;
+}
+
+//Star Rating//
+
+function setStarRating() {
+    if (moves > 16) {
+        let thirdStar = document.getElementById("star3")
+        thirdStar.children[0].classList.add("star-white");
+    }
+    if (moves > 25) {
+        let secondStar = document.getElementById("star2")
+        secondStar.children[0].classList.add("star-white");
+
+    }
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+//Reset Stars//
+function resetStars() {
+
+    let thirdStar = document.getElementById("star3");
+    let secondStar = document.getElementById("star2");
+    let firstStar = document.getElementById("star1");
+    thirdStar.children[0].classList.remove("star-white");
+    secondStar.children[0].classList.remove("star-white");
+    firstStar.children[0].classList.remove("star-white");
+
+}
+
+
+// Handle selected cards//
+//Matching cards//
+async function handleMatchedCards() {
+
+    await delay(500);
+    matchingSelection.push(...currentSelection);
+
+    currentSelection = [];
+    matchingSelection.forEach(card => {
+        card.classList.add("match");
+        card.classList.remove("open", "show");
+        goodShake(card);
+    });
+
+}
+
+//Non-matching cards//
+async function handleUnmatchedCards() {
+    await delay(500);
+    currentSelection.forEach(card => {
+        badShake(card);
+    });
+    await delay(500);
+    currentSelection.forEach(card => {
+
+        card.classList.remove("open", "show");
+
+    });
+    currentSelection = [];
+}
+
+
+// Animation of Compared cards//
+
+function badShake(card) {
+    // perform a horizontal shake when the cards match for 1sec//
+    card.classList.add("animated", "wobble");
+}
+
 function goodShake(card) {
     // perform a vertical shake when the cards match for 1sec//
     card.classList.add("animated", "hop");
 }
 
+//Finish Game//
 function gameDone() {
-    console.log("Game Done")
-    console.log("length of matched card array" + matchingSelection.length)
+
     if (matchingSelection.length == 16) {
-        console.log("waiting for pop up")
+
+        stopTimer();
         popMessage();
+        matchingSelection = [];
     }
-    matchingSelection = [];
+}
+
+
+// Create a pop-message//
+
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+let btn = document.getElementById("button");
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+/* When the user clicks anywhere outside of the modal, close it*/
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+
 }
 function clickButton() {
     document.getElementById("button").click();
 }
 
 function popMessage() {
-    console.log("popup works")
+
     document.getElementById("moves").innerText = moves;
-    document.getElementById("time").innerText = "TBD";
+    document.getElementById("time").innerText = displayTime;
     clickButton();
 }
 
+// Create a timer//
+function pad(n) {
+    return ('00' + n).slice(-2);
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:*/
+function stopTimer() {
+    clearInterval(timer);
+    timerStarted = false;
+}
+
+
+function startTimer() {
+    timerStarted = true;
+    let timerElement = document.getElementById("timer")
+    startTime = Date.now();
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+
+    timer = setInterval(function () {
+        let currentTime = Date.now();
+        let difference = currentTime - startTime;
+
+        let hours = pad((difference / hour) | 0);
+        let minutes = pad(((difference % hour) / minute) | 0);
+        let seconds = pad(((difference % minute) / second) | 0);
+        displayTime = hours + ':' + minutes + ':' + seconds;
+        timerElement.innerHTML = displayTime;
+    }, 250);
+}
+
+function restartTimer() {
+    stopTimer();
+    let timerElement = document.getElementById("timer");
+    timerElement.innerText = "";
+    
+
+}
+
+
+/* set up the event listener for a card. If a card is clicked:*/
 function playGame() {
     let allCards = document.querySelectorAll('.card');
     allCards.forEach(card => {
         card.addEventListener('click', async function (e) {
 
-            console.log(e.srcElement.children[0].className);
+            if (timerStarted == false) {
+                startTimer();
+            }
 
             if (checkIfCardSelected(e.srcElement) == false && checkTwoCardLimit() == false) {
                 e.srcElement.className = 'card open show';
                 currentSelection.push(e.srcElement);
             }
+
             if (currentSelection.length == 2) {
                 incrementMove();
                 if (compareCards() == true) {
                     await handleMatchedCards();
-                    console.log(matchingSelection.srcElement);
+
                 } else {
                     await handleUnmatchedCards();
                 }
@@ -199,31 +290,17 @@ restartElement.addEventListener('click', function (evt) {
     playGame();
 });
 
+// refresh Game//
 function refresh() {
-    console.log('The restart button was clicked');
+
     moves = -1;
     incrementMove();
     shuffle(deck);
     createNewDeck();
-}
+    stopTimer()
 
-let modal = document.getElementById("myModal");
-let span = document.getElementsByClassName("close")[0];
-let btn = document.getElementById("button");
-
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+    restartTimer();
+    resetStars();
 }
 
 playGame();
